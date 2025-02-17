d8afabf95bc314ed8a60fd083b1477db8ac20d4e
anasazizi05 <155018289+anasazizi05@users.noreply.github.com>
2025-02-18 00:52:51 +0100

    Update main.tf

--- modules/Google-Storage/main.tf
+++ modules/Google-Storage/main.tf
@@ -1,12 +1,40 @@
-# *** YOUR CODE HERE ***
-# Crear un recurso google_storage_bucket para almacenar imágenes en Google Storage
-#
-# Crear en el bucket anteior dos recursos google_storage_bucket_object
-# (uno para cada imagen de la carpeta ./images)
-# Configurar la propiedad source desde la raíz del proyecto (./images/<imagen.jpg>)
-#
-# Definir una regla de acceso para objeto creado de forma que sea público 
-# Configurar google_storage_object_access_control así
-#   role   = "READER"
-#   entity = "allUsers"
-# **********************  
+resource "google_storage_bucket" "image_bucket" {
+  name     = var.gcp_bucket_name
+  location = "US"
+  storage_class = "STANDARD"       
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365
+    }
+  }
+}
+
+resource "google_storage_bucket_object" "image1" {
+  name   = "el_enigma_de_la_habitacion_622.jpg"
+  bucket = google_storage_bucket.image_bucket.name
+  source = "./images/el_enigma_de_la_habitacion_622.jpg"
+}
+
+resource "google_storage_bucket_object" "image2" {
+  name   = "una_historia_de_espana.jpg"
+  bucket = google_storage_bucket.image_bucket.name
+  source = "./images/una_historia_de_espana.jpg"
+}
+
+resource "google_storage_object_access_control" "image1_access" {
+  bucket = google_storage_bucket.image_bucket.name
+  object = google_storage_bucket_object.image1.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_object_access_control" "image2_access" {
+  bucket = google_storage_bucket.image_bucket.name
+  object = google_storage_bucket_object.image2.name
+  role   = "READER"
+  entity = "allUsers"
+}
