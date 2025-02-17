3d6b626c19e2235a55869b5249f36851ffaed6c8
anasazizi05 <155018289+anasazizi05@users.noreply.github.com>
2025-02-18 00:57:45 +0100

    Update variables.tf

--- modules/Google-Storage/variables.tf
+++ modules/Google-Storage/variables.tf
@@ -1,7 +1,14 @@
-# *** YOUR CODE HERE ***
-# Definir 3 variables
-# * gcp_username inicializada con el nombre de usuario en GCP
-# * gcp_project inicializada con el nombre del proyecto en GCP
-# * gcp_bucket_name inicializada con el nombre del bucket a crear en Google Storage
-# **********************
+variable "gcp_username" {
+  description = "El nombre de usuario en Google Cloud Platform (GCP)."
+  default     = "anas" 
+}
 
+variable "gcp_project" {
+  description = "El ID del proyecto en Google Cloud Platform (GCP)."
+  default     = "anas-444616" 
+}
+
+variable "gcp_bucket_name" {
+  description = "El nombre del bucket a crear en Google Storage."
+  default     = "imagen_buckets"
+}
